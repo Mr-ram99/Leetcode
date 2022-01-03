23bb4f763da2e6dd94bf6141f21caa48f16dd671
Jabra Ram <jrchoudhary2410@gmail.com>
2022-01-03 22:54:07 +0530

    Time: 0 ms (100.00%), Space: 39.3 MB (7.05%) - LeetHub

--- binary-tree-preorder-traversal/binary-tree-preorder-traversal.java
+++ binary-tree-preorder-traversal/binary-tree-preorder-traversal.java
@@ -15,20 +15,34 @@
  */
 class Solution {
     public List<Integer> preorderTraversal(TreeNode root) {
+//         iterative
+    //     List<Integer> list = new ArrayList<>();
+    //     if(root==null)
+    //         return list;
+    //     Stack<TreeNode> st = new Stack<>();
+    //     st.push(root);
+    //     while(!st.isEmpty())
+    //     {
+    //         TreeNode temp = st.pop();
+    //         list.add(temp.val);
+    //         if(temp.right!=null)
+    //             st.push(temp.right);
+    //         if(temp.left!=null)
+    //             st.push(temp.left);
+    //     }
+    //     return list;
+    // }
+//         recursive
         List<Integer> list = new ArrayList<>();
-        if(root==null)
-            return list;
-        Stack<TreeNode> st = new Stack<>();
-        st.push(root);
-        while(!st.isEmpty())
-        {
-            TreeNode temp = st.pop();
-            list.add(temp.val);
-            if(temp.right!=null)
-                st.push(temp.right);
-            if(temp.left!=null)
-                st.push(temp.left);
-        }
+        helper(root,list);
         return list;
+    }
+    public static void helper(TreeNode root, List<Integer> list)
+    {
+        if(root == null)
+            return ;
+        list.add(root.val);
+        helper(root.left,list);
+        helper(root.right,list);
     }
 }
