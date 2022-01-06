42c1dd048603b8205f46575236ca308b5fe4af20
Jabra Ram <jrchoudhary2410@gmail.com>
2022-01-06 14:52:59 +0530

    Time: 12 ms (12.19%), Space: 41.6 MB (11.46%) - LeetHub

--- 1094-car-pooling/1094-car-pooling.java
+++ 1094-car-pooling/1094-car-pooling.java
@@ -1,15 +1,7 @@
 class Solution {
     public boolean carPooling(int[][] trips, int capacity) {
-        int i,pass=trips[0][0];
-        int min = trips[0][1],max=trips[0][1];
-        for(i=0;i<trips.length;i++)
-        {
-            if(trips[i][1]<min)
-                min = trips[i][1];
-            if(trips[i][2]>max)
-                max = trips[i][2];
-        }
-        int [] arr = new int[max+1];
+        int i;
+        int [] arr = new int[1001];
         for(i=0;i<trips.length;i++)
         {
             for(int j=trips[i][1];j<trips[i][2];j++)
