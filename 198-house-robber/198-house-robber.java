83d212bab1393d79f9caaa47503609291cc58290
Jabra Ram <jrchoudhary2410@gmail.com>
2022-01-07 18:44:22 +0530

    Time: 1 ms (7.55%), Space: 38 MB (19.54%) - LeetHub

--- 198-house-robber/198-house-robber.java
+++ 198-house-robber/198-house-robber.java
@@ -1,14 +1,28 @@
 class Solution {
     public int rob(int[] nums) {
-        int [] dp = new int[nums.length];
-        dp[0] = nums[0];
+//         with space
+        // int [] dp = new int[nums.length];
+        // dp[0] = nums[0];
+        // if(nums.length==1)
+        //     return nums[0];
+        // dp[1] = Math.max(nums[0],nums[1]);
+        // for(int i=2;i<nums.length;i++)
+        // {
+        //     dp[i] = Math.max(dp[i-2]+nums[i],dp[i-1]);
+        // }
+        // return dp[dp.length-1];
+//         without space
+        int dp = nums[0];
         if(nums.length==1)
             return nums[0];
-        dp[1] = Math.max(nums[0],nums[1]);
+        int dp1 = Math.max(nums[0],nums[1]);
+        int dp2 = dp1;;
         for(int i=2;i<nums.length;i++)
         {
-            dp[i] = Math.max(dp[i-2]+nums[i],dp[i-1]);
+            dp2 = Math.max(dp+nums[i],dp1);
+            dp = dp1;
+            dp1 = dp2;
         }
-        return dp[dp.length-1];
+        return dp2;
     }
 }
