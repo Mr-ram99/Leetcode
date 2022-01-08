df6fbd8b949af69cd6d8731e3ad68dae4487b11b
Jabra Ram <jrchoudhary2410@gmail.com>
2022-01-08 22:10:53 +0530

    Time: 14 ms (33.33%), Space: 62 MB (33.33%) - LeetHub

--- 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.java
+++ 2130-maximum-twin-sum-of-a-linked-list/2130-maximum-twin-sum-of-a-linked-list.java
@@ -10,39 +10,60 @@
  */
 class Solution {
     public int pairSum(ListNode head) {
-        if(head.next.next==null)
-            return head.val+head.next.val;
+//        stack
         ListNode slow = head;
         ListNode fast = head;
+        Stack<Integer> stk = new Stack<>();
         while(fast!=null && fast.next!=null)
         {
+            stk.push(slow.val);
             slow = slow.next;
             fast = fast.next.next;
         }
-        ListNode mid = reverse(slow);
         int max=0;
-        while(mid!=null)
+        while(!stk.isEmpty())
         {
-            if(max<mid.val+head.val)
-                max = mid.val + head.val;
-            mid = mid.next;
-            head= head.next;
+            int i = stk.pop()+slow.val;
+            if(max<i)
+                max = i;
+            slow = slow.next;
         }
         return max;
-    }
-    public static ListNode reverse(ListNode node)
-    {
-        ListNode pre = null;
-        ListNode cur = node;
-        ListNode nxt = cur.next;
-        while(nxt!=null)
-        {
-            cur.next = pre;
-            pre = cur;
-            cur = nxt;
-            nxt = nxt.next;
-        }
-        cur.next = pre;
-        return cur;
+        
+//         reverse half and iterate
+    //     if(head.next.next==null)
+    //         return head.val+head.next.val;
+    //     ListNode slow = head;
+    //     ListNode fast = head;
+    //     while(fast!=null && fast.next!=null)
+    //     {
+    //         slow = slow.next;
+    //         fast = fast.next.next;
+    //     }
+    //     ListNode mid = reverse(slow);
+    //     int max=0;
+    //     while(mid!=null)
+    //     {
+    //         if(max<mid.val+head.val)
+    //             max = mid.val + head.val;
+    //         mid = mid.next;
+    //         head= head.next;
+    //     }
+    //     return max;
+    // }
+    // public static ListNode reverse(ListNode node)
+    // {
+    //     ListNode pre = null;
+    //     ListNode cur = node;
+    //     ListNode nxt = cur.next;
+    //     while(nxt!=null)
+    //     {
+    //         cur.next = pre;
+    //         pre = cur;
+    //         cur = nxt;
+    //         nxt = nxt.next;
+    //     }
+    //     cur.next = pre;
+    //     return cur;
     }
 }
