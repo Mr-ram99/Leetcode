a222b17ee49aee719865de276cbf949d4a1894fa
Jabra Ram <jrchoudhary2410@gmail.com>
2022-01-11 16:48:34 +0530

    Time: 0 ms (100.00%), Space: 36.4 MB (59.59%) - LeetHub

--- 198-house-robber/198-house-robber.java
+++ 198-house-robber/198-house-robber.java
@@ -1,28 +1,18 @@
 class Solution {
     public int rob(int[] nums) {
-//         with space
-        // int [] dp = new int[nums.length];
-        // dp[0] = nums[0];
-        // if(nums.length==1)
-        //     return nums[0];
-        // dp[1] = Math.max(nums[0],nums[1]);
-        // for(int i=2;i<nums.length;i++)
-        // {
-        //     dp[i] = Math.max(dp[i-2]+nums[i],dp[i-1]);
-        // }
-        // return dp[dp.length-1];
-//         without space
-        int dp = nums[0];
         if(nums.length==1)
             return nums[0];
-        int dp1 = Math.max(nums[0],nums[1]);
-        int dp2 = dp1;;
+        if(nums.length<=2)
+            return nums[0]>nums[1]?nums[0]:nums[1];
+        int prev = nums[0];
+        int prev2 = nums[0]>nums[1]?nums[0]:nums[1];
+        int cur = 0;     //value doesn't matter
         for(int i=2;i<nums.length;i++)
         {
-            dp2 = Math.max(dp+nums[i],dp1);
-            dp = dp1;
-            dp1 = dp2;
+            cur = Math.max(prev2,prev+nums[i]);
+            prev = prev2;
+            prev2 = cur;
         }
-        return dp2;
+        return cur;
     }
 }
