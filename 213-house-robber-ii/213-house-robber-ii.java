3134239c9982e6af98fee3ee18c7579e2e70f842
Jabra Ram <jrchoudhary2410@gmail.com>
2022-01-11 17:37:45 +0530

    Time: 0 ms (100.00%), Space: 36.7 MB (39.05%) - LeetHub

--- 213-house-robber-ii/213-house-robber-ii.java
+++ 213-house-robber-ii/213-house-robber-ii.java
@@ -2,29 +2,44 @@ class Solution {
     public int rob(int[] nums) {
         if(nums.length==1)
             return nums[0];
-        if(nums.length<=2)
+        if(nums.length==2)
             return nums[0]>nums[1]?nums[0]:nums[1];
-        int max=0;
-        for(int i=0;i<nums.length;i++)
+        if(nums.length==3)
         {
-            int temp =helper(nums,i);
-            if(max<temp)
-                max = temp;
+            int m = Math.max(nums[0],nums[1]);
+            return (int)Math.max(m,nums[2]);
         }
-        return max;
+        int temp1 =helper(nums,0);
+        int temp2 =helper(nums,1);
+        return (int)Math.max(temp1,temp2);
     }
     public static int helper(int[] nums,int k)
     {
         int n = nums.length;
-        int prev = nums[k];
-        int prev2 = nums[k]>nums[(k+1)%n]?nums[k]:nums[(k+1)%n];
-        int cur = prev2;
-        for(int i=k+2,j=0;j<n-3;i++,j++)
+        int prev,prev2,cur=0;
+        if(k==0)
         {
-            i=i%n;
-            cur = Math.max(prev2,prev+nums[i]);
-            prev = prev2;
-            prev2 = cur;
+            prev = nums[0];
+            prev2 = nums[1]>nums[0]?nums[1]:nums[0];
+            cur = prev2;
+            for(int i=2;i<n-1;i++)
+            {
+                cur = Math.max(prev+nums[i],prev2);
+                prev = prev2;
+                prev2 =cur;
+            }
+        }
+        if(k==1)
+        {
+            prev = nums[1];
+            prev2 = nums[1]>nums[2]?nums[1]:nums[2];
+            cur = prev2;
+            for(int i=3;i<n;i++)
+            {
+                cur = Math.max(prev+nums[i],prev2);
+                prev = prev2;
+                prev2 =cur;
+            }
         }
         return cur;
     }
