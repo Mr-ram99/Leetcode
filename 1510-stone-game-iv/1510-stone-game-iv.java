f2511c473f4e46c3c46cefc4228fd26250e7d455
Jabra Ram <jrchoudhary2410@gmail.com>
2022-01-22 21:44:08 +0530

    Time: 17 ms (73.99%), Space: 37.9 MB (70.52%) - LeetHub

--- 1510-stone-game-iv/1510-stone-game-iv.java
+++ 1510-stone-game-iv/1510-stone-game-iv.java
@@ -1,39 +1,54 @@
+// class Solution {
+//     public boolean winnerSquareGame(int n) {
+//         if((int)(Math.sqrt(n))*(int)(Math.sqrt(n)) == n)
+//             return true;
+//         HashSet<Integer> set = new HashSet<>();
+//         set.add(1);
+//         int [] dp = new int[n+1];
+//         dp[1] = 1;
+//         int i;
+//         for(i=2;i<=n;i++)
+//         {
+//             if((int)Math.sqrt(i)*(int)Math.sqrt(i) == i)
+//                 set.add(i);
+//             if(canAliceWin(i,dp,set))
+//                 dp[i] = 1;
+//             else
+//                 dp[i] = 2;
+//         }
+//         if(dp[n]==1)
+//             return true;
+//         return false;
+//     }
+//     public static boolean canAliceWin(int n, int[] dp, HashSet<Integer> set)
+//     {
+//         if((int)(Math.sqrt(n))*(int)(Math.sqrt(n)) == n)
+//             return true;
+//         int j=n;
+//         for(int k:set)
+//         {
+//             int index = n-k;
+//             if(index>0)
+//             {
+//                 if(dp[index]==2)
+//                     return true;
+//             }
+//         }
+//         return false;
+//     }        
+// }
+
 class Solution {
     public boolean winnerSquareGame(int n) {
-        if((int)(Math.sqrt(n))*(int)(Math.sqrt(n)) == n)
-            return true;
-        HashSet<Integer> set = new HashSet<>();
-        set.add(1);
-        int [] dp = new int[n+1];
-        dp[1] = 1;
-        int i;
-        for(i=2;i<=n;i++)
-        {
-            if((int)Math.sqrt(i)*(int)Math.sqrt(i) == i)
-                set.add(i);
-            if(canAliceWin(i,dp,set))
-                dp[i] = 1;
-            else
-                dp[i] = 2;
-        }
-        if(dp[n]==1)
-            return true;
-        return false;
-    }
-    public static boolean canAliceWin(int n, int[] dp, HashSet<Integer> set)
-    {
-        if((int)(Math.sqrt(n))*(int)(Math.sqrt(n)) == n)
-            return true;
-        int j=n;
-        for(int k:set)
-        {
-            int index = n-k;
-            if(index>0)
-            {
-                if(dp[index]==2)
-                    return true;
+        boolean[] dp = new boolean[n + 1];
+        for (int i = 0; i < n + 1; i++) {
+            for (int k = 1; k * k <= i; k++) {
+                if (dp[i - k * k] == false) {
+                    dp[i] = true;
+                    break;
+                }
             }
         }
-        return false;
-    }        
+        return dp[n];
+    }
 }
