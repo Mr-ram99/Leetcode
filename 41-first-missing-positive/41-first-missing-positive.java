2a407c84349320433e2a4e7da9e4816d95bc2d38
Jabra Ram <jrchoudhary2410@gmail.com>
2022-01-22 22:43:43 +0530

    Time: 9 ms (26.66%), Space: 96.8 MB (64.16%) - LeetHub

---
+++ 41-first-missing-positive/41-first-missing-positive.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int firstMissingPositive(int[] nums) {
+        int ci, i, max = Integer.MIN_VALUE;
+        boolean containszero = false;
+        HashSet<Integer> set = new HashSet<>();
+        for(i=0;i<nums.length;)
+        {
+            if(nums[i]==0)
+                containszero=true;
+            if(nums[i]>0 && nums[i]<nums.length){
+                ci = nums[i]-1;
+                if(i!=ci && !set.contains(nums[i])){
+                    set.add(nums[i]);
+                    int temp = nums[i];
+                    nums[i] = nums[ci];
+                    nums[ci] = temp;
+                    }
+                else i++;
+            }  
+            else i++;
+        }
+        for(i=0;i<nums.length;i++)
+        {
+            if(nums[i]!=i+1)
+                return i+1;
+        }
+        return nums.length+1;
+    }
+}
