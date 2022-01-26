e3f9211a306a0b001400932b71778233ad1ee502
Jabra Ram <jrchoudhary2410@gmail.com>
2022-01-26 18:37:23 +0530

    Time: 30 ms (32.61%), Space: 86.9 MB (25.72%) - LeetHub

--- 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.java
+++ 1305-all-elements-in-two-binary-search-trees/1305-all-elements-in-two-binary-search-trees.java
@@ -15,18 +15,43 @@
  */
 class Solution {
     public List<Integer> getAllElements(TreeNode root1, TreeNode root2) {
-        List<Integer> list = new ArrayList<>();
-        getnode(root1,list);
-        getnode(root2,list);
-        Collections.sort(list);
-        return list;
+        List<Integer> list1 = new ArrayList<>();
+        List<Integer> list2 = new ArrayList<>();
+        List<Integer> ans = new ArrayList<>();
+        getnode(root1,list1);
+        getnode(root2,list2);
+        int i=0,j=0;
+        while(i<list1.size() && j<list2.size())
+        {
+            if(list1.get(i)<list2.get(j))
+            {
+                ans.add(list1.get(i));
+                i++;
+            }
+            else
+            {
+                ans.add(list2.get(j));
+                j++;
+            }
+        }
+        while(i<list1.size())
+        {
+            ans.add(list1.get(i));
+            i++;
+        }
+        while(j<list2.size())
+        {
+            ans.add(list2.get(j));
+            j++;
+        }
+        return ans;
     }
-    public static void getnode(TreeNode root, List<Integer> list)
+    public static void getnode(TreeNode root, List<Integer> li)
     {
         if(root == null)
             return ;
-        list.add(root.val);
-        getnode(root.left,list);
-        getnode(root.right,list);
+        getnode(root.left,li);
+        li.add(root.val);
+        getnode(root.right,li);
     }
 }
