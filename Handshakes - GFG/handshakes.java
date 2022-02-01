e2f9fd5bdea11136d31826be7f24b573ccee085e
Jabra Ram <jrchoudhary2410@gmail.com>
2022-02-01 16:01:52 +0530

    Added solution - LeetHub

--- Handshakes - GFG/handshakes.java
+++ Handshakes - GFG/handshakes.java
@@ -26,22 +26,37 @@ class Solution{
     static int count(int N) 
     { 
         // code here
+        // ------------- dp approach ---O(n2)
         if(N<=2)
             return N/2;
-        int [] dp = new int[N/2+1];
-        dp[0] = 1;
-        dp[1] = 1;
-        int sum=0;
-        for(int i=2;i<=N/2;i++)
-        {
-            sum =0;
-            for(int j=i-1;j>=0;j--)
-            {
-                sum += dp[j]*dp[i-j-1];
-            }
-            dp[i] = sum;
-        }
-        return dp[N/2];
+        // int [] dp = new int[N/2+1];
+        // dp[0] = 1;
+        // dp[1] = 1;
+        // int sum=0;
+        // for(int i=2;i<=N/2;i++)
+        // {
+        //     sum =0;
+        //     for(int j=i-1;j>=0;j--)
+        //     {
+        //         sum += dp[j]*dp[i-j-1];
+        //     }
+        //     dp[i] = sum;
+        // }
+        // return dp[N/2];
+        //  -----------------
+        // ---------------------O(n) approach
+        int n = N/2;
+        long ans = binomial(2*n, n);
+        return (int)(ans/(n+1));
     }
-    
+    public static long binomial(int m, int n)
+    {
+        long res = 1;
+        for(int i=0;i<n;i++)
+        {
+           res *= (m-i);
+           res /= (i+1);
+        }  
+        return res;
+    }
 };
