355f21e733a81a1d777c3e44fe70e4a665079259
Jabra Ram <jrchoudhary2410@gmail.com>
2022-02-07 22:24:45 +0530

    Added solution - LeetHub

---
+++ Postorder Traversal (Iterative) - GFG/postorder-traversal-iterative.java
@@ -0,0 +1,136 @@
+// { Driver Code Starts
+// Initial Template for Java
+
+import java.util.LinkedList;
+import java.util.Queue;
+import java.io.*;
+import java.util.*;
+
+class Node {
+    int data;
+    Node left;
+    Node right;
+    Node(int data) {
+        this.data = data;
+        left = null;
+        right = null;
+    }
+}
+
+class GfG {
+
+    static Node buildTree(String str) {
+
+        if (str.length() == 0 || str.charAt(0) == 'N') {
+            return null;
+        }
+
+        String ip[] = str.split(" ");
+        // Create the root of the tree
+        Node root = new Node(Integer.parseInt(ip[0]));
+        // Push the root to the queue
+
+        Queue<Node> queue = new LinkedList<>();
+
+        queue.add(root);
+        // Starting from the second element
+
+        int i = 1;
+        while (queue.size() > 0 && i < ip.length) {
+
+            // Get and remove the front of the queue
+            Node currNode = queue.peek();
+            queue.remove();
+
+            // Get the current node's value from the string
+            String currVal = ip[i];
+
+            // If the left child is not null
+            if (!currVal.equals("N")) {
+
+                // Create the left child for the current node
+                currNode.left = new Node(Integer.parseInt(currVal));
+                // Push it to the queue
+                queue.add(currNode.left);
+            }
+
+            // For the right child
+            i++;
+            if (i >= ip.length) break;
+
+            currVal = ip[i];
+
+            // If the right child is not null
+            if (!currVal.equals("N")) {
+
+                // Create the right child for the current node
+                currNode.right = new Node(Integer.parseInt(currVal));
+
+                // Push it to the queue
+                queue.add(currNode.right);
+            }
+            i++;
+        }
+
+        return root;
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br =
+            new BufferedReader(new InputStreamReader(System.in));
+
+        int t = Integer.parseInt(br.readLine());
+
+        while (t > 0) {
+            String s = br.readLine();
+            Node root = buildTree(s);
+            Tree g = new Tree();
+            ArrayList<Integer> res = g.postOrder(root);
+            for (int i = 0; i < res.size(); i++)
+                System.out.print(res.get(i) + " ");
+            System.out.print("\n");
+            t--;
+        }
+    }
+}
+
+// Position this line where user code will be pasted.
+// } Driver Code Ends
+
+
+// User function Template for Java
+
+/*
+class Node{
+    int data;
+    Node left;
+    Node right;
+    Node(int data){
+        this.data = data;
+        left=null;
+        right=null;
+    }
+}
+*/
+class Tree {
+    ArrayList<Integer> postOrder(Node root) {
+        // code here
+        Stack<Node> st1 = new Stack<Node>(); 
+        Stack<Node> st2 = new Stack<Node>();
+        ArrayList<Integer> postOrder = new ArrayList<Integer>();
+        
+        if(root == null) return postOrder; 
+        
+        st1.push(root); 
+        while(!st1.isEmpty()) {
+            root = st1.pop(); 
+            st2.add(root); 
+            if(root.left != null) st1.push(root.left); 
+            if(root.right != null) st1.push(root.right); 
+        }
+        while(!st2.isEmpty()) {
+            postOrder.add(st2.pop().data); 
+        }
+        return postOrder; 
+    }
+}
