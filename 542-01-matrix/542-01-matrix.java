c17c4d9e9b80bef047735b17af77d9b280094714
Jabra Ram <jrchoudhary2410@gmail.com>
2022-02-07 21:51:50 +0530

    Time: 5 ms (99.31%), Space: 46.1 MB (60.98%) - LeetHub

---
+++ 542-01-matrix/542-01-matrix.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int[][] updateMatrix(int[][] mat) {
+        int n = mat.length;
+        int m = mat[0].length;
+        int [][] dist = new int[n][m];
+        
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                dist[i][j] = Integer.MAX_VALUE - 100000;
+            }
+        }
+        
+       for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (mat[i][j] == 0) {
+                    dist[i][j] = 0;
+                } else {
+                    if (i > 0)
+                        dist[i][j] = Math.min(dist[i][j], dist[i - 1][j] + 1);
+                    if (j > 0)
+                        dist[i][j] = Math.min(dist[i][j], dist[i][j - 1] + 1);
+                }
+            }
+        }
+
+        //Second pass: check for bottom and right
+        for (int i = n - 1; i >= 0; i--) {
+            for (int j = m - 1; j >= 0; j--) {
+                if (i < n - 1)
+                    dist[i][j] = Math.min(dist[i][j], dist[i + 1][j] + 1);
+                if (j < m - 1)
+                    dist[i][j] = Math.min(dist[i][j], dist[i][j + 1] + 1);
+            }
+        }
+        return dist;
+    }
+  
+}
