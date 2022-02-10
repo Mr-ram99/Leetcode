778c5898f79313772132bb905ad39ea365262ac2
Jabra Ram <jrchoudhary2410@gmail.com>
2022-02-10 17:52:07 +0530

    Time: 0 ms (100.00%), Space: 38.6 MB (21.80%) - LeetHub

--- 70-climbing-stairs/70-climbing-stairs.java
+++ 70-climbing-stairs/70-climbing-stairs.java
@@ -1,14 +1,9 @@
 class Solution {
     public int climbStairs(int n) {
-        int prev = 0;
-        int prev2 = 1;
-        int cur = 1;
-        for(int i=1;i<=n;i++)
-        {
-            cur = prev + prev2;
-            prev = prev2;
-            prev2 = cur;
-        }
-        return cur;
+        if(n==1 || n==2)
+            return n;
+        double phi = (1+Math.sqrt(5))/2;
+        double PHI = (1-Math.sqrt(5))/2;
+        return (int)((Math.pow(phi,n+1)-Math.pow(PHI,n+1))/Math.sqrt(5));
     }
 }
