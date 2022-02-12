03d8b581845cdf9a745223f4e766a145950bf194
Jabra Ram <jrchoudhary2410@gmail.com>
2022-02-12 21:29:30 +0530

    Time: 1 ms (66.87%), Space: 43.5 MB (15.33%) - LeetHub

---
+++ 82-remove-duplicates-from-sorted-list-ii/82-remove-duplicates-from-sorted-list-ii.java
@@ -0,0 +1,20 @@
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        ListNode dummy = new ListNode(0, head);
+        ListNode pred = dummy;
+        
+        while (head != null) {
+            if (head.next != null && head.val == head.next.val) {
+                while (head.next != null && head.val == head.next.val) {
+                    head = head.next;    
+                }
+                pred.next = head.next;     
+            } else {
+                pred = pred.next;    
+            }
+                
+            head = head.next;    
+        }  
+        return dummy.next;
+    }
+}
