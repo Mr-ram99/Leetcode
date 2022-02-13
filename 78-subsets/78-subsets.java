fef462136631d97d04d36681454d28fb00514e53
Jabra Ram <jrchoudhary2410@gmail.com>
2022-02-13 12:55:10 +0530

    Time: 0 ms (100.00%), Space: 42.1 MB (25.40%) - LeetHub

---
+++ 78-subsets/78-subsets.java
@@ -0,0 +1,17 @@
+class Solution {
+    public List<List<Integer>> subsets(int[] nums) {
+        List<List<Integer>> list = new ArrayList<>();
+        List<Integer> li = new ArrayList<>();
+        list.add(li);
+        for(int i=0;i<nums.length;i++)
+        {
+            int size = list.size();
+            for(int j=0;j<size;j++){
+                List<Integer> temp = new ArrayList<>(list.get(j));
+                temp.add(nums[i]);
+                list.add(temp);
+            }
+        }
+        return list;
+    }
+}
