50c0659074bf577af63abc2779f9ae41b9b63bf7
Jabra Ram <jrchoudhary2410@gmail.com>
2022-02-21 14:40:37 +0530

    Time: 2 ms (82.16%), Space: 56.1 MB (18.73%) - LeetHub

---
+++ 169-majority-element/169-majority-element.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int majorityElement(int[] nums) {
+        int maj=nums[0], count=1;
+        for(int i=1;i<nums.length;i++){
+            if(nums[i]==maj)
+                count++;
+            else 
+                count--;
+            if(count==0)
+            {
+                maj = nums[i+1];
+                i++;
+                count=1;
+            }
+        }
+        return maj;
+    }
+}
