2b7d983cf4f6875290e9a1eb53e0426aad68af68
Jabra Ram <jrchoudhary2410@gmail.com>
2022-02-23 23:26:57 +0530

    Added solution - LeetHub

--- Minimize the Heights II - GFG/minimize-the-heights-ii.java
+++ Minimize the Heights II - GFG/minimize-the-heights-ii.java
@@ -34,16 +34,14 @@ import java.util.*;
 class Solution {
     int getMinDiff(int[] arr, int n, int k) {
         // code here
-         int min =0, max =0, ans =0;
         Arrays.sort(arr);
-        ans = arr[n-1]-arr[0];
-        for(int i =1;i<n;i++){
-           if(arr[i]>=k){
-            max = Math.max(arr[n-1]-k,arr[i-1]+k);
-            min = Math.min(arr[i]-k,arr[0]+k);
-            ans = Math.min(ans,max-min);
-           }
-          
+        int ans = arr[n-1] - arr[0];
+        for(int i=1;i<n;i++){
+            if(arr[i]>=k){
+                int max = Math.max(arr[n-1]-k,arr[i-1]+k);
+                int min = Math.min(arr[i]-k, arr[0]+k);
+                ans = Math.min(ans,max-min);
+            }
         }
         return ans;
     }
