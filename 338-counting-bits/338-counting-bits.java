b2415d13a6023e4d03d483aa007a6732155ca3c1
Jabra Ram <jrchoudhary2410@gmail.com>
2022-03-01 09:54:18 +0530

    Time: 5 ms (28.89%), Space: 48.8 MB (7.76%) - LeetHub

---
+++ 338-counting-bits/338-counting-bits.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int[] countBits(int n) {
+        int [] arr = new int[n+1];
+        for(int i=0;i<=n;i++){
+            int count=0;
+            int k=i;
+            while(k>0){
+                if((k&1)==1)
+                    count++;
+                k>>=1;
+            }
+            arr[i]=count;
+        }
+        return arr;
+    }
+}
