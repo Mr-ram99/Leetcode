63d8582f4e8b2e46e18061729159fee69678fcc7
Jabra Ram <jrchoudhary2410@gmail.com>
2022-03-02 21:39:48 +0530

    Time: 3 ms (93.64%), Space: 52.4 MB (94.25%) - LeetHub

---
+++ 11-container-with-most-water/11-container-with-most-water.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int maxArea(int[] height) {
+        int max=0;
+        for(int i=0,j=height.length-1;i<=j;){
+            max = Math.max(max, (j-i)*Math.min(height[i],height[j]));
+            if(height[i]>height[j])
+                           j--;
+            else
+                           i++;
+        }
+        return max;
+    }
+}
