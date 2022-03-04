65eaeefb472093bcf3c62f81f5b29072491b28cb
Jabra Ram <jrchoudhary2410@gmail.com>
2022-03-04 22:49:49 +0530

    Time: 6 ms (77.17%), Space: 44.7 MB (52.06%) - LeetHub

--- 799-champagne-tower/799-champagne-tower.java
+++ 799-champagne-tower/799-champagne-tower.java
@@ -3,7 +3,9 @@ class Solution {
         int r = query_row;
         float [][] dp = new float[r+1][r+1];
         dp[0][0]=(float)poured;
+        boolean flag;
         for(int i=1;i<=r;i++){
+            flag=false;
             for(int j=0;j<=i;j++){
                 if(j==0){
                     if(dp[i-1][j]>1)
@@ -19,9 +21,11 @@ class Solution {
                     if(dp[i-1][j]>1)
                         dp[i][j] += (dp[i-1][j]-1)/2.0; 
                 }
-                System.out.print(dp[i][j]+"  ");
+                if(dp[i][j]>1)
+                    flag=true;
             }
-            System.out.println();
+            if(!flag)
+                break;
         }
         if(dp[query_row][query_glass]>1.0)
             return 1.0;
