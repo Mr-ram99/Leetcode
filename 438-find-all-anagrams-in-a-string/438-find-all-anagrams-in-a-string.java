9b6137acacdf6d3a06906344e1b87c3c01b3e641
Jabra Ram <jrchoudhary2410@gmail.com>
2022-03-05 21:44:05 +0530

    Time: 12 ms (71.08%), Space: 48.1 MB (34.46%) - LeetHub

--- 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.java
+++ 438-find-all-anagrams-in-a-string/438-find-all-anagrams-in-a-string.java
@@ -1,52 +1,42 @@
 class Solution {
     public List<Integer> findAnagrams(String s, String p) {
-        List<Integer> res = new ArrayList<>();
-        int len_p = p.length();
-        int len_s = s.length();
         
-        if(len_s==0 || len_p==0 || len_p>len_s)
-            return res;
-        
-        int i, start = 0, end = 0, diff=len_p;
-        char ch;
         int [] chars = new int[26];
-        
-        for(i=0;i<len_p;i++)
-        {
-            ch = p.charAt(i);
-            chars[ch-'a']++;
+        int i, j, diff=p.length();
+        List<Integer> ans = new ArrayList<>();
+        if(s.length()<p.length())
+            return ans;
+        for(i=0;i<p.length();i++){
+            chars[p.charAt(i)-'a']++;
         }
-        
-        while(end<len_p)
-        {
-            ch = s.charAt(end);
-            chars[ch-'a']--;
-            
-            if(chars[ch-'a']>=0)
+        for(i=0;i<p.length();i++){
+            chars[s.charAt(i)-'a']--;
+            if(chars[s.charAt(i)-'a']>=0)
                 diff--;
-            end++;
         }
         if(diff==0)
-            res.add(0);
-        
-        while(end<len_s)
-        {
-            ch = s.charAt(start);
-            if(chars[ch-'a']>=0)
+            ans.add(0);
+        j=0;
+        while(i<s.length()){
+            char c = s.charAt(i);
+            char d = s.charAt(j);
+            if(chars[d-'a']>=0)
                 diff++;
-            chars[ch-'a']++;
-            start++;
+                chars[d-'a']++;
+            j++;
             
-            ch = s.charAt(end);
-            chars[ch-'a']--;
-            if(chars[ch-'a']>=0)
+            chars[c-'a']--;
+            if(chars[c-'a']>=0)
                 diff--;
             
-            end++;
+            i++;
             
             if(diff==0)
-                res.add(start);
+                ans.add(j);
+            
+            
         }
-    return res;  
+        return ans;
+        
     }
 }
