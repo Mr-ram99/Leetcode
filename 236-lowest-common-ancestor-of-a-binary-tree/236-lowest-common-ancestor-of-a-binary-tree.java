135a8af413860d3148fbfafa549172b78363e0f0
Jabra Ram <jrchoudhary2410@gmail.com>
2022-03-07 21:42:44 +0530

    Time: 12 ms (38.62%), Space: 47.5 MB (26.59%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.java
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.java
@@ -9,25 +9,35 @@
  */
 class Solution {
     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
-        ArrayList<TreeNode> list1 = new ArrayList<>();
-        ArrayList<TreeNode> list2 = new ArrayList<>();
-        path(root, p, list1);
-        path(root, q, list2);
-        int i=0;
-        while(i<list1.size() && i<list2.size() && list1.get(i)==list2.get(i)){
-            i++;
+        if(root==null || root==p || root==q){
+            return root;
         }
-        return list1.get(i-1);
-    }
-    public static boolean path(TreeNode root, TreeNode t, ArrayList<TreeNode> list){
-        if(root==null)
-            return false;
-        list.add(root);
-        if(root == t)
-            return true;
-        if(path(root.left,t,list) || path(root.right,t,list))
-            return true;
-        list.remove(list.size()-1);
-        return false;
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if(left==null)
+            return right;
+        if(right==null)
+            return left;
+        return root;
+    //     ArrayList<TreeNode> list1 = new ArrayList<>();
+    //     ArrayList<TreeNode> list2 = new ArrayList<>();
+    //     path(root, p, list1);
+    //     path(root, q, list2);
+    //     int i=0;
+    //     while(i<list1.size() && i<list2.size() && list1.get(i)==list2.get(i)){
+    //         i++;
+    //     }
+    //     return list1.get(i-1);
+    // }
+    // public static boolean path(TreeNode root, TreeNode t, ArrayList<TreeNode> list){
+    //     if(root==null)
+    //         return false;
+    //     list.add(root);
+    //     if(root == t)
+    //         return true;
+    //     if(path(root.left,t,list) || path(root.right,t,list))
+    //         return true;
+    //     list.remove(list.size()-1);
+    //     return false;
     }
 }
