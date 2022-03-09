2e9943a4badc75b39c404c46fb7bef0dd71d9768
Jabra Ram <jrchoudhary2410@gmail.com>
2022-03-09 19:59:45 +0530

    Time: 16 ms (22.94%), Space: 55.1 MB (8.25%) - LeetHub

--- 784-letter-case-permutation/784-letter-case-permutation.java
+++ 784-letter-case-permutation/784-letter-case-permutation.java
@@ -1,24 +1,32 @@
 class Solution {
     public List<String> letterCasePermutation(String s) {
         List<String> list = new ArrayList<>();
-        permute("",s,list);
+        permute("", s, 0, list);
         return list;
     }
-    public static void permute(String p, String s, List<String> list)
-    {
-        int i=0;
-        if(s.length()==0)
+    public static void permute(String p, String s, int i, List<String> list){
+        
+        while(i<s.length() && Character.isDigit(s.charAt(i))){
+            p += s.charAt(i);
+            i++;
+        }
+        if(p.length()==s.length())
         {
             list.add(p);
             return;
         }
-        char ch = s.charAt(0);
-        if(Character.isDigit(ch))
-            permute(p+ch,s.substring(1),list);
-        else
-        {
-            permute(p+Character.toLowerCase(ch),s.substring(1),list);
-            permute(p+Character.toUpperCase(ch),s.substring(1),list);
+        
+        if(65<=s.charAt(i) && s.charAt(i)<=90){
+            char ch = s.charAt(i);
+            char ch1 = (char)(ch+32);
+            permute(p+ch, s, i+1, list);
+            permute(p+ch1, s, i+1, list);
         }
-    }
+         else {
+            char ch = s.charAt(i);
+            char ch1 = (char)(ch-32);
+            permute(p+ch, s, i+1, list);
+            permute(p+ch1, s, i+1, list);
+         } 
+    } 
 }
