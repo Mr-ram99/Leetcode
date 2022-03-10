1b835f7e0f492e7eb1099e93da49fcb3e39476cd
Jabra Ram <jrchoudhary2410@gmail.com>
2022-03-10 19:25:35 +0530

    Time: 3 ms (73.45%), Space: 48.5 MB (7.12%) - LeetHub

--- 2-add-two-numbers/2-add-two-numbers.java
+++ 2-add-two-numbers/2-add-two-numbers.java
@@ -10,63 +10,60 @@
  */
 class Solution {
     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        if(l1 == null)
+            return l2;
+        if(l2 == null)
+            return l1;
+        ListNode head = new ListNode();
+        ListNode cur = head;
         int sum=0, carry=0;
-        ListNode temp = new ListNode();
-        ListNode head = temp;
-        while(l1!=null && l2!=null)
-        {
-           sum = l1.val + l2.val + carry;
-            if(sum>=10)
-            {
-                sum-=10;
-                carry=1;
+        while(l1!=null && l2!=null){
+            sum = l1.val + l2.val + carry;
+            if(sum>=10){
+                sum -= 10;
+                carry =1;
             }
-            else 
+            else{
                 carry=0;
-            ListNode cur = new ListNode(sum);
-            temp.next = cur;
-            temp = cur;
+            }
+            ListNode temp = new ListNode(sum);
+            cur.next = temp;
+            cur = temp;
+            
             l1 = l1.next;
             l2 = l2.next;
         }
-        while(l1!=null)
-        {
+        while(l1!=null){
             sum = l1.val + carry;
-            if(sum>=10)
-            {
-                sum-=10;
-                carry=1;
+            if(sum>=10){
+                sum -= 10;
+                carry =1;
             }
-            else 
-            {
-                carry = 0;
-            }
-            ListNode cur = new ListNode(sum);
-            temp.next = cur;
-            temp = cur;
-            l1 = l1.next;
-        }
-        while(l2!=null)
-        {
-            sum = l2.val + carry;
-            if(sum>=10)
-            {
-                sum-=10;
-                carry=1;
-            }
-            else 
-            {
+            else{
                 carry=0;
             }
-            ListNode cur = new ListNode(sum);
-            temp.next = cur;
-            temp = cur;
+            ListNode temp = new ListNode(sum);
+            cur.next = temp;
+            cur = temp;
+            l1 = l1.next;
+        }
+        while(l2!=null){
+            sum = l2.val + carry;
+            if(sum>=10){
+                sum -= 10;
+                carry =1;
+            }
+            else{
+                carry=0;
+            }
+            ListNode temp = new ListNode(sum);
+            cur.next = temp;
+            cur = temp;
             l2 = l2.next;
         }
-        if(carry==1)
-        {
-            ListNode cur = new ListNode(1);
-            temp.next = cur;
+        if(carry == 1){
+            ListNode temp = new ListNode(1);
+            cur.next = temp;
         }
         return head.next;
     }
