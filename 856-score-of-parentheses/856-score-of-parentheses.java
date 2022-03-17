57290ef1649d33c2fb2ea47c78e7fcd947a50656
Jabra Ram <jrchoudhary2410@gmail.com>
2022-03-17 14:58:43 +0530

    Time: 0 ms (100.00%), Space: 42.1 MB (33.27%) - LeetHub

--- 856-score-of-parentheses/856-score-of-parentheses.java
+++ 856-score-of-parentheses/856-score-of-parentheses.java
@@ -1,16 +1,28 @@
 class Solution {
     public int scoreOfParentheses(String s) {
-        Stack<Integer> stk = new Stack<>();
-        stk.push(0);
-        for(char ch: s.toCharArray()){
-            if(ch=='(')
-                stk.push(0);
+        // Stack<Integer> stk = new Stack<>();
+        // stk.push(0);
+        // for(char ch: s.toCharArray()){
+        //     if(ch=='(')
+        //         stk.push(0);
+        //     else{
+        //         int x = stk.pop();
+        //         int y = stk.pop();
+        //         stk.push(y+Math.max(2*x,1));
+        //     }
+        // }
+        // return stk.pop();
+        
+        int ans=0, depth = 0;
+        for(int i=0;i<s.length();i++){
+            if(s.charAt(i)=='(')
+                depth++;
             else{
-                int x = stk.pop();
-                int y = stk.pop();
-                stk.push(y+Math.max(2*x,1));
+                depth--;
+                if(s.charAt(i-1)=='(')
+                    ans += 1<<depth;
             }
         }
-        return stk.pop();
+        return ans;
     }
 }
