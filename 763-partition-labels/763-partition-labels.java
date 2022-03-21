3fdbcbc53f146d48d4f609ded6b404c1df8eb799
Jabra Ram <jrchoudhary2410@gmail.com>
2022-03-21 17:08:26 +0530

    Time: 8 ms (41.06%), Space: 42.7 MB (44.15%) - LeetHub

--- 763-partition-labels/763-partition-labels.java
+++ 763-partition-labels/763-partition-labels.java
@@ -1,25 +1,33 @@
 class Solution {
     public List<Integer> partitionLabels(String s) {
-        int [] lastOcc = new int[26];
-        int st=0, e=0;
-        List<Integer> res = new ArrayList<>();
-        for(int i=0;i<s.length();i++)
-        {
+        List<Integer> list = new ArrayList<>();
+        int n=s.length();
+        int [] start = new int[26];
+        int [] end = new int[26];
+        Arrays.fill(start,-1);
+        Arrays.fill(end,-1);
+        for(int i=0;i<n;i++){
             char ch = s.charAt(i);
-            lastOcc[ch-'a'] = i;
+            if(start[ch-'a']!=-1)
+                start[ch-'a']=i;
         }
-        for(int i=0;i<s.length();i++)
-        {
+        for(int i=0;i<n;i++){
             char ch = s.charAt(i);
-            if(i>e)
-            {
-                res.add(e-st+1);
-                st = i;
+            end[ch-'a']=i;
+        }
+        int st=0, e=end[s.charAt(0)-'a'];
+        for(int i=0;i<n;i++){
+            char ch = s.charAt(i);
+            if(e==i){
+                list.add(e-st+1);
+                st=i+1;
+                if(i+1<n)
+                    e = end[s.charAt(i+1)-'a'];
             }
-            e = Math.max(e, lastOcc[ch-'a']);
+            else if(end[ch-'a']>e){
+                e = end[ch-'a'];
+            }
         }
-        res.add(e-st+1);
-        return res;
-        
+        return list;
     }
 }
