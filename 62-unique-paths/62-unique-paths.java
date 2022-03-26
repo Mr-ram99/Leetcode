1691fc77c72055e483ccfe2b8ad9e51f895cdae5
Jabra Ram <jrchoudhary2410@gmail.com>
2022-03-26 19:48:31 +0530

    Time: 0 ms (100.00%), Space: 41.8 MB (7.93%) - LeetHub

--- 62-unique-paths/62-unique-paths.java
+++ 62-unique-paths/62-unique-paths.java
@@ -1,17 +1,12 @@
 class Solution {
     public int uniquePaths(int m, int n) {
-        if(m==1 || n==1)
-            return 1;
-        int [][] dp = new int[m+1][n+1];
-        return calc(m,n,dp);
-    }
-    public static int calc(int m, int n, int [][] dp)
-    {
-        if(m==1 || n==1)
-            return 1;
-        if(dp[m][n]!=0)
-            return dp[m][n];
-        dp[m][n] = calc(m-1,n,dp)+calc(m,n-1,dp);
-        return dp[m][n];
+        long ans=1;
+        long min = (long)Math.min(m,n);
+        long j=1;
+        for(int i=m+n-2;j<min;i--,j++){
+            ans *= i;
+            ans /= j;
+        }
+        return (int)ans;
     }
 }
