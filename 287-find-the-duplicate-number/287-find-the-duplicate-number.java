ed49695183c85967888dd5e0fedcc085234c2a0b
Jabra Ram <jrchoudhary2410@gmail.com>
2022-03-29 18:59:32 +0530

    Time: 7 ms (59.05%), Space: 75.7 MB (51.17%) - LeetHub

--- 287-find-the-duplicate-number/287-find-the-duplicate-number.java
+++ 287-find-the-duplicate-number/287-find-the-duplicate-number.java
@@ -1,20 +1,20 @@
 class Solution {
-    public int findDuplicate(int[] arr) {
-        int cr  = 0;
-        for(int i=0;i<arr.length;i++){
-            cr = arr[i]-1;
-            if(arr[cr]!=arr[i]){
-                int temp = arr[cr];
-                arr[cr] = arr[i];
-                arr[i] = temp;
+    public int findDuplicate(int[] nums) {
+        int correctIndex;
+        for(int i=0;i<nums.length;i++){
+            correctIndex = nums[i]-1;
+            if(nums[correctIndex]!=nums[i]){
+                int temp = nums[correctIndex];
+                nums[correctIndex] = nums[i];
+                nums[i]=temp;
                 i--;
             }
         }
-        for(int i=0;i<arr.length;i++){
-            if(arr[i]!=i+1){
-                return arr[i];
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]!=i+1){
+                return nums[i];
             }
         }
-        return -1; // dummy return
+        return -1;
     }
 }
