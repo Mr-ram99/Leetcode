05fd70c0a65bdc66150197c4fb5ffc730de23a35
Jabra Ram <jrchoudhary2410@gmail.com>
2022-03-31 22:30:13 +0530

    Time: 2 ms (39.75%), Space: 41.3 MB (75.59%) - LeetHub

---
+++ 410-split-array-largest-sum/410-split-array-largest-sum.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int splitArray(int[] nums, int m) {
+        int low = 0, high = 0, min = Integer.MAX_VALUE;
+        for(int i=0;i<nums.length;i++){
+            low = Math.max(low, nums[i]);
+            high += nums[i];
+        }
+        while(low <= high) {
+            int mid = (low + high) / 2;
+            if(required_no_of_chunks(mid, m, nums)){
+               min = Math.min(min, mid);
+               high = mid - 1;
+            }
+            else low = mid + 1;
+        }
+        return min;
+    }
+    
+    private boolean required_no_of_chunks(int mid, int m, int[] nums){
+        int chunks = 0, i=0;
+        while(i < nums.length){
+            int val = 0;
+            while(i < nums.length && nums[i] + val <= mid) val += nums[i++];
+            chunks++;
+        }
+        return chunks <= m;
+    }
+}
